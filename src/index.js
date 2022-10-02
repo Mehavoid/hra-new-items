e546fafc4a5ed89595b973cfd66f0b0c40cda6d8
Mehavoid <tontole@riseup.net>
2022-10-02 18:10:33 +0300

    Add base functional of script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hi world');
+const { Date } = H;
+
+const create = (name) => R.createElement(name);
+const createT = (str) => R.createTextNode(str);
+const append = (a, b) => a.append(b);
+const before = (a, b) => a.insertBefore(b, a.lastChild);
+const print = (target, time) => {
+  const p = create('p');
+  const s = create('strong');
+  const text = createT(time);
+  const space = createT('\u00A0');
+  s.textContent = 'Available:';
+  append(p, s);
+  append(p, space);
+  append(p, text);
+  before(target.offsetParent, p);
+};
+
+const now = Date.now();
+const releases = R.querySelectorAll('a.btn-addtocart');
+
+for (const release of releases) {
+  const { id } = release.dataset;
+  if (!id) continue;
+  const time = await GM.getValue(id, now);
+  if (time === now) await GM.setValue(id, now);
+  print(release, time);
+}
