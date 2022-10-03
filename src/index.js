f1e2d604463c5db24152a20f0046ee6d2085d8d7
Mehavoid <tontole@riseup.net>
2022-10-03 18:36:37 +0300

    Prune a release that is more than one month old

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 'use strict';
 
-const { Date } = H;
+const MONTH = 30;
+const MSEC_PER_DAY = 1000 * 60 * 60 * 24;
+
+const { Date, Math } = H;
+
+const utc = (t) => {
+  const d = new Date(t);
+  const day = d.getDate();
+  const year = d.getFullYear();
+  const month = d.getMonth();
+  return Date.UTC(year, month, day);
+};
+const diff = (t1, t2) => {
+  const now = utc(t1);
+  const then = utc(t2);
+  return Math.floor((then - now) / MSEC_PER_DAY);
+};
+const compare = (d1, d2) => d1 >= d2;
 
 const create = (name) => R.createElement(name);
 const createT = (str) => R.createTextNode(str);
@@ -26,5 +43,6 @@ for (const release of releases) {
   if (!id) continue;
   const time = await GM.getValue(id, now);
   if (time === now) await GM.setValue(id, now);
+  else if (compare(diff(time, now), MONTH)) await GM.deleteValue(id);
   print(release, time);
 }
